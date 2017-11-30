4e19828762efb5ab369958243067f92e801aeaf1
Kevin Harrington <mad.hephaestus@gmail.com>
2017-11-30 14:49:07 -0500

    adding the cad generation script

--- Bowler/cadEngine.groovy
+++ Bowler/cadEngine.groovy
@@ -1,1 +1,77 @@
-//Your code here
+import com.neuronrobotics.bowlerstudio.creature.ICadGenerator;
+import com.neuronrobotics.bowlerstudio.creature.CreatureLab;
+import org.apache.commons.io.IOUtils;
+import com.neuronrobotics.bowlerstudio.vitamins.*;
+import java.nio.file.Paths;
+import eu.mihosoft.vrl.v3d.FileUtil;
+import javafx.scene.transform.*;
+println "Loading STL file"
+// Load an STL file from a git repo
+// Loading a local file also works here
+
+return new ICadGenerator(){
+	@Override 
+	public ArrayList<CSG> generateCad(DHParameterKinematics d, int linkIndex) {
+		File legFile = null
+		if(linkIndex ==0){
+			legFile = ScriptingEngine.fileFromGit(
+			"https://github.com/keionbis/SmallKat.git",
+			"STLs/Elbow Joint.STL");
+
+		}
+		if(linkIndex ==1){
+			legFile = ScriptingEngine.fileFromGit(
+			"https://github.com/keionbis/SmallKat.git",
+			"STLs/Midleg.STL");
+
+		}
+
+		if(linkIndex ==2){
+			legFile = ScriptingEngine.fileFromGit(
+			"https://github.com/keionbis/SmallKat.git",
+			"STLs/Lower Leg.STL");
+
+		}
+
+		ArrayList<DHLink> dhLinks = d.getChain().getLinks()
+		DHLink dh = dhLinks.get(linkIndex)
+		// Hardware to engineering units configuration
+		LinkConfiguration conf = d.getLinkConfiguration(linkIndex);
+		// Engineering units to kinematics link (limits and hardware type abstraction)
+		AbstractLink abstractLink = d.getAbstractLink(linkIndex);// Transform used by the UI to render the location of the object
+		// Transform used by the UI to render the location of the object
+		Affine manipulator = dh.getListener();
+
+		ArrayList<CSG> allCad=new ArrayList<>();
+		// Load the .CSG from the disk and cache it in memory
+		CSG body  = Vitamins.get(legFile)
+
+		body.setManipulator(manipulator);
+		body.setColor(javafx.scene.paint.Color.WHITE)
+		def parts = [body ] as ArrayList<CSG>
+		for(int i=0;i<parts.size();i++){
+			parts.get(i).setColor(javafx.scene.paint.Color.GRAY)
+		}
+		return parts;
+		
+	}
+	@Override 
+	public ArrayList<CSG> generateBody(MobileBase b ) {
+		ArrayList<CSG> allCad=new ArrayList<>();
+
+		File mainBodyFile = ScriptingEngine.fileFromGit(
+			"https://github.com/keionbis/SmallKat.git",
+			"STLs/Body.STL");
+
+		// Load the .CSG from the disk and cache it in memory
+		CSG body  = Vitamins.get(mainBodyFile)
+
+		body.setManipulator(b.getRootListener());
+		body.setColor(javafx.scene.paint.Color.WHITE)
+		def parts = [body ] as ArrayList<CSG>
+		for(int i=0;i<parts.size();i++){
+			parts.get(i).setColor(javafx.scene.paint.Color.GRAY)
+		}
+		return parts;
+	}
+};
