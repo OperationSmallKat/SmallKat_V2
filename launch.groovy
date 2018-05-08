02945aa035723fcda09029d437995503f7b6b80e
Kevin Harrington <mad.hephaestus@gmail.com>
2018-05-08 12:09:03 -0400

    adding prints of trajectory values

--- launch.groovy
+++ launch.groovy
@@ -331,8 +331,8 @@ def cat =DeviceManager.getSpecificDevice( "jaguar",{
 def gameController = ScriptingEngine.gitScriptRun(
             "https://gist.github.com/e26c0d8ef7d5283ef44fb22441a603b8.git", // git location of the library
             "LoadGameController.groovy" , // file to load
-            // Parameters passed to the funcetion
-            ["Game*"]
+            // Parameters passed to the function
+            ["GameController_22"]
             )
 if(gameController==null){
 	return 
@@ -340,7 +340,7 @@ if(gameController==null){
 
 byte [] data = gameController.getData() 
 double toSeconds=0.03//100 ms for each increment
-          
+
 while (!Thread.interrupted()){
 	Thread.sleep((toSeconds*1000))
 	double xdata = data[0]
@@ -349,9 +349,10 @@ while (!Thread.interrupted()){
 		xdata+=256
 	if(rzdata<0)
 		rzdata+=256
-		
-	double displacement = 5.0*xdata/255.0
-	double rot = 5.0*rzdata/255.0
+	double scale = 5.0
+	double displacement = scale*xdata/255.0-scale/2
+	double rot =scale*rzdata/255.0-scale/2
+	println "displacement "+displacement+" rot "+rot
 	TransformNR move = new TransformNR(displacement,0,0,new RotationNR(0,rot,0))
 	cat.DriveArc(move, toSeconds);
 }
