7a1e2e21ce4de341621853d397ce2679942b550a
Kevin Harrington <mad.hephaestus@gmail.com>
2018-05-25 15:33:24 -0400

    corrected maximum step size

--- Bowler/DynamicWalking.groovy
+++ Bowler/DynamicWalking.groovy
@@ -40,13 +40,13 @@ if(args==null){
 	}
 	boolean usePhysicsToMove = true;
 	long stepCycleTime =5000
-	int numStepCycleGroups = 4
+	int numStepCycleGroups = 2
 	double standardHeadTailAngle = -20
 	double staticPanOffset = 10
 	double coriolisGain = 1
 	boolean headStable = false
-	double maxBodyDisplacementPerStep = 20
-	double minBodyDisplacementPerStep = 10
+	double maxBodyDisplacementPerStep = 40
+	double minBodyDisplacementPerStep = 20
 	args =  [stepOverHeight,
 	stepOverTime,
 	zLock,
