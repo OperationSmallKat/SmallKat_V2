762763106a71b85a5aef0d2b3bc4840a825a6955
Kevin Harrington <mad.hephaestus@gmail.com>
2018-05-28 16:19:12 -0400

    updating urls

--- loadRobot.groovy
+++ loadRobot.groovy
@@ -180,33 +180,4 @@ def cat =DeviceManager.getSpecificDevice( "MediumKat",{
 	return m
 })
 
-def gameController = ScriptingEngine.gitScriptRun(
-            "https://gist.github.com/e26c0d8ef7d5283ef44fb22441a603b8.git", // git location of the library
-            "LoadGameController.groovy" , // file to load
-            // Parameters passed to the function
-            ["GameController*"]
-            )
-         
-if(gameController==null){
-
-	return 
-}
-
-byte [] data = gameController.getData() 
-double toSeconds=0.03//100 ms for each increment
-
-while (!Thread.interrupted()){
-	Thread.sleep((long)(toSeconds*1000))
-	double xdata = data[2]
-	double rzdata = data[1]
-	if(xdata<0)
-		xdata+=256
-	if(rzdata<0)
-		rzdata+=256
-	double scale = 1.0
-	double displacement = scale*xdata/255.0-scale/2
-	double rot =scale*rzdata/255.0-scale/2
-	println "displacement "+displacement+" rot "+rot
-	TransformNR move = new TransformNR(displacement,0,0,new RotationNR(0,rot,0))
-	cat.DriveArc(move, toSeconds);
-}
+return cat
