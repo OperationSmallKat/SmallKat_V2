c0f3122dd89a4434de8a45641288a3c3c96e8fba
Kevin Harrington <mad.hephaestus@gmail.com>
2018-06-02 22:53:02 -0400

    adding parameters to the launch script call of the robot loader

--- launch.groovy
+++ launch.groovy
@@ -1,5 +1,8 @@
 
-def cat =ScriptingEngine.gitScriptRun("https://github.com/keionbis/SmallKat.git", "loadRobot.groovy", null);
+def cat =ScriptingEngine.gitScriptRun(	"https://github.com/keionbis/SmallKat.git", 
+								"loadRobot.groovy", 
+["https://github.com/keionbis/SmallKat.git",
+		"Bowler/MediumKat.xml"]);
 
 def gameController = ScriptingEngine.gitScriptRun(
             "https://gist.github.com/e26c0d8ef7d5283ef44fb22441a603b8.git", // git location of the library
