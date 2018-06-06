0a5bc24317cd3586b607cc6b77ae2e5f733288f7
Kevin Harrington <mad.hephaestus@gmail.com>
2018-06-05 22:06:38 -0400

    adding tilt control

--- launch.groovy
+++ launch.groovy
@@ -17,22 +17,38 @@ if(gameController==null){
 }
 
 byte [] data = gameController.getData() 
-double toSeconds=0.1//100 ms for each increment
+double toSeconds=0.03//100 ms for each increment
 
 while (!Thread.interrupted()){
 	Thread.sleep((long)(toSeconds*1000))
-	double xdata = data[2]
-	double rzdata = data[1]
+	double xdata = data[4]
+	double rzdata = data[3]
+	double rxdata = data[1]
+	double rydata = data[2]
 	if(xdata<0)
 		xdata+=256
 	if(rzdata<0)
 		rzdata+=256
+	if(rxdata<0)
+		rxdata+=256
+	if(rydata<0)
+		rydata+=256
 	double scale = 1.0
 	double displacement = 15*(scale*xdata/255.0-scale/2)
-	double rot =(scale*rzdata/255.0-scale/2)*5
-	if(Math.abs(displacement)>0.1 || Math.abs(rot)>0.1){
+	double rot =((scale*rzdata/255.0)-scale/2)*5
+	double rotx =((rxdata/255.0)-scale/2)*10
+	double roty =((rydata/255.0)-scale/2)*10
+	if(Math.abs(displacement)<0.1 ){
+		displacement=0
+	}
+	if( Math.abs(rot)<0.1){
+		rot=0
+	}
+	if(Math.abs(displacement)>0.1 || Math.abs(rot)>0.1||
+	Math.abs(rotx)>0.1 || Math.abs(roty)>0.1){
 		println "displacement "+displacement+" rot "+rot
-		TransformNR move = new TransformNR(displacement,0,0,new RotationNR(0,rot,0))
+		println "tilt "+rotx+" rot "+roty
+		TransformNR move = new TransformNR(displacement,0,0,new RotationNR(rotx,rot,roty))
 		cat.DriveArc(move, toSeconds);
 	}
 }
