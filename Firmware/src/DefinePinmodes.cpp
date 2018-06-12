d973a3733e3d8fbee61fbe4e6192de9f8756ab34
keionbis <keion96@gmail.com>
2018-06-12 10:28:21 -0400

    Moved Firmware

--- Firmware/src/DefinePinmodes.cpp
+++
@@ -1,7 +0,0 @@
-//
-// Created by keionbis on 11/9/17.
-//
-
-void SetupPinModes(){
-
-}
--- Firmware/src/SupportingFunctions.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by keionbis on 11/16/17.
-//
-#include "main.h"
-
-long map(long x, long in_min, long in_max, long out_min, long out_max)
-{
-    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
