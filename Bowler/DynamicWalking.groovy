32912d7f5aee8b3fe3aada181d09bcdc0b3b391b
keionbis <keion96@gmail.com>
2018-11-30 09:54:56 -0500

    Updated head pinout

--- Bowler/DynamicWalking.groovy
+++ Bowler/DynamicWalking.groovy
@@ -19,7 +19,7 @@ import com.neuronrobotics.sdk.addons.kinematics.imu.*
 
 
 if(args==null){
-	double stepOverHeight=10;
+	double stepOverHeight=15;
 	long stepOverTime=20*5*2;// Servo loop times number of points times Nyquest doubeling
 	Double zLock=-3;
 	Closure calcHome = { DHParameterKinematics leg -> 
